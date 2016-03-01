8706830c9b265397ce2b289f6d6f3c65408efe75
abhinav4192 <abhinav.hsr@gmail.com>
2016-03-01 21:55:16 +0530

    version 7
    Bugfixes.
    Popup bug still pending

--- app/src/main/java/fightingpit/barrons1100/DatabaseContract.java
+++ app/src/main/java/fightingpit/barrons1100/DatabaseContract.java
@@ -6,7 +6,7 @@ import android.provider.BaseColumns;
  * Created by AG on 07-Oct-15.
  */
 public class DatabaseContract {
-    public static final  int    DATABASE_VERSION    = 3;
+    public static final  int    DATABASE_VERSION    = 5;
     public static final  String DATABASE_NAME       = "database.db";
     private static final String COMMA_SEP           = ", ";
 
